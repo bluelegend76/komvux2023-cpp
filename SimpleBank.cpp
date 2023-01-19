4491122a8514abde48abccd5420faa0983d5e3c5
Bluelegend76 <trulystrange@gmail.com>
2023-01-19 23:37:16 +0100

    First Succesful Dummy Version SimpleBank

--- SimpleBank.cpp
+++ SimpleBank.cpp
@@ -5,51 +5,6 @@ using namespace std;
 int main() {
   cout << "Welcome to SimpleBank." << endl;
 
-  // {{{
-  // while (true)
-  // {
-  //   // Print the menu:
-  //   cout << "[D]eposit: \n";
-  //   cout << "[W]ithdraw\n";
-  //   cout << "[A]ccount Balance\n";
-  //   cout << "[C]alculate Interest-payment\n";
-  //   cout << "[Q]uit\n";
-
-  //   char menuSelection;  // the user's choice
-  //   cin >> menuSelection;
-
-  //   if (menuSelection == 'D' || menuSelection == 'd')
-  //   {
-  //     cout << "(choice is Deposit)\n";
-  //     // Code goes here
-  //   }
-  //   else if (menuSelection == 'W' || menuSelection == 'w')
-  //   {
-  //     cout << "(choice is Withdraw)\n";
-  //     // Code goes here
-  //   }
-  //   else if (menuSelection == 'A' || menuSelection == 'a')
-  //   {
-  //     cout << "(choice is Account Balance)\n";
-  //     // Code goes here
-  //   }
-  //   else if (menuSelection == 'C' || menuSelection == 'c')
-  //   {
-  //     cout << "(choice is Calculate Interest-payment)\n";
-  //     // Code goes here
-  //   }
-  //   else if (menuSelection == 'Q' || menuSelection == 'q')
-  //   {
-  //     break;  // avbryter while-loopen
-  //   }
-  //   else
-  //   {
-  //     cout << "Unsupported choice! Try again.\n";
-  //   }
-  // }
-  // }}}
-
-  // {{{
   // Print menu
   cout << "[D]eposit\n";
   cout << "[W]ithdraw\n";
@@ -89,7 +44,6 @@ int main() {
         break;
     }
   }
-  // }}}
 
   // cin.get()
   return 0;
